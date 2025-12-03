7cfd66fa9e7ced730f62022ccfd310edfcae555f
James-Roe-Intercede <james.roe@intercede.com>
2025-12-03 11:30:34 +0000

    Move Intercede Logging to Legacy Logs

--- Yubico.Core/src/Yubico/Core/Logging/Log.Legacy.cs
+++ Yubico.Core/src/Yubico/Core/Logging/Log.Legacy.cs
@@ -36,6 +36,86 @@ namespace Yubico.Core.Logging
         }
 
         [Obsolete("Obsolete, use equivalent ILogger method, or view the changelog for further instruction.")]
-        public static Logger GetLogger() => new Logger(Yubico.Core.Logging.Log.LoggerFactory.CreateLogger("Yubico.Core logger"));
+        /// <summary>
+        /// Gets an instance of the active logger, bypassing the factory
+        /// </summary>
+        public static Logger GetLogger()
+        {   
+            //Return a logger working in two different ways, depending on whether a LoggerFactory has been set
+            if (!(_factory == null || _factory is NullLoggerFactory))
+            {
+                return new Logger(LoggerFactory.CreateLogger("Yubico.Core logger"));
+            }
+            return new Logger("WDAYubiKey-{0}");
+        }
+
+        /// <summary>
+        /// Gets an instance of the underlying logfile object
+        /// </summary>
+        public static Logfile GetLogFile(string name, bool overwrite)
+        {
+            // Ensure the logs collection and log file is created once
+            lock (_lock)
+            {
+                // First time we are getting a logfile
+                if (_logs.Count == 0)
+                {
+                    // In .net core, we need to register the additional code pages we use here
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                }
+
+                // If logfile with this product name and unformatted filename is not found, then create it and add it to the collection of logs
+                if (!_logs.TryGetValue(name, out var logfile))
+                {
+                    // If we havent managed to get a valid value, create a blank non-logging logfile object instead
+                    if (string.IsNullOrEmpty(_loggingFolder))
+                    {
+                        logfile = new Logfile();
+                    }
+                    else
+                    {
+                        // Determine the logfile name
+                        // Format e.g. AuthlogicsAuthenticationServerManager-{0}.log
+                        //Name should contain the location for the date string in parameter 0 ie {0}
+                        var now = DateTime.Now;
+                        var logFileNameOutput = string.Format(name, $"{now.Year}{now.Month}{now.Day}{now.Hour}{now.Minute}{now.Second}");
+
+                        logfile = new Logfile(logFileNameOutput, _loggingFolder, _loggingEnabled, overwrite)
+                        {
+                            SafeLog = true
+                        };
+                    }
+
+                    // Write out the version so we know what version of the Authlogics.dll we have on the system
+                    logfile.AddVersion();
+
+                    _logs.Add(name, logfile);
+                }
+
+                return logfile;
+            }
+        }
+
+        private static void GetProcessSettings()
+        {
+            var path = "SOFTWARE\\Authlogics\\Windows Desktop Agent\\";
+            var registry = new Registry(path);
+
+            try
+            {
+                var folder = registry.GetValue("LoggingFolder", RegistryValueKind.String).ToString();
+                if (!string.IsNullOrEmpty(folder)) _loggingFolder = folder;
+
+                var value = registry.GetValue("LoggingEnabled", RegistryValueKind.DWord, false);
+                if (value.ToString() == "1")
+                {
+                    _loggingEnabled = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Cant log anything here as we are in the logging class
+            }
+        }
     }
 }
